ea63a1e14b85eb6d55cfda291f4c0cbd58823704
Klassikcat <shinjeongtae@gmail.com>
2022-10-23 23:15:58 +0900

    add linked list

---
+++ src/linked_list.py
@@ -0,0 +1,73 @@
+from typing import *
+import logging
+
+
+class Node(object):
+    def __init__(self, item: Any = None, next: ClassVar = None, is_head: bool = False, is_tail: bool = False) -> None:
+        self.item = item
+        self.next = next
+        self.is_head = is_head
+        self.is_tail = is_tail
+
+    def __call__(self) -> Any:
+        return self.item
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, value) -> ClassVar:
+        self.next = value
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.tail = Node(is_tail=True)
+        self.head = Node(is_head=True, next=self.tail)
+        self.length = 2
+
+    def remove(self, idx: int):
+        prev_node = self[idx-1]
+        next_node = self[idx+1]
+        prev_node.next = next_node
+        self.length -= 1
+
+    def append(self, item: Any, idx: int):
+        node_new = Node(item)
+        prev_node = self[idx-1]
+        next_node = prev_node.next
+        prev_node.next = node_new
+        node_new.set_next(next_node)
+        self.length += 1
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        current_node = self.head
+        for idx in range(idx + 1):
+            current_node = current_node.next
+        return current_node
+
+
+def __main__():
+    linked_list = LinkedList()
+
+    linked_list.append('a', 0)
+    linked_list.append('b', 1)
+    linked_list.append('d', 2)
+    linked_list.append('e', 3)
+    linked_list.append('f', 4)
+    idx_2_init = linked_list[2].item
+
+    linked_list.append('c', 2)
+
+    idx_2_inserted = linked_list[2].item
+
+    linked_list.remove(2)
+    idx_2_removed = linked_list[2].item
+
+    logging.info(f"initial item at idx 2: {idx_2_init}\nafter insert item at idx 2: {idx_2_inserted}\nafter remove item at idx 2: {idx_2_removed}")
+
+
+if __name__ == "__main__":
+    __main__()
